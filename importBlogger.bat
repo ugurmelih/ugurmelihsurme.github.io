0d51d21226c47ae1831caf086a9161fcef82aa89
ugurmelihsurme <ugurmelihsurme@gmail.com>
2020-05-27 13:36:26 +0300

    jekyll build is added.

---
+++ importBlogger.bat
@@ -0,0 +1,6 @@
+$ ruby -r rubygems -e 'require "jekyll-import";
+    JekyllImport::Importers::Blogger.run({
+      "source"                => "blog-05-27-2020.xml",
+      "no-blogger-info"       => false, # not to leave blogger-URL info (id and old URL) in the front matter
+      "replace-internal-link" => false, # replace internal links using the post_url liquid tag.
+    })'
